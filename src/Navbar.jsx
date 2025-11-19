50d933088dd9302d7d6b0fa5fbcfbff1cda20e8d
Teja2142 <gktechnologies.stl@gmail.com>
2025-11-19 12:41:36 +0530

    login register updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -55,6 +55,18 @@ const Navbar = () => {
                 Contact
               </Link>
             </li>
+            {/* Register */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+            </li>
+            {/* Login */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
           </ul>
 
           {/* CTA Button */}
